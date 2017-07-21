8d63b67a8693fa3948588e414eb3c636ce0a6b2f
m4dz <code@m4dz.net>
2017-07-21 10:58:01 +0200

    [feat] add TOTP auth mock

--- data/env_standalone.js
+++ data/env_standalone.js
@@ -1,5 +1,5 @@
 module.exports = Object.assign(require('./env.js'), {
   NODE_ENV: 'standalone',
-  COZY_FIELDS: `{"connector": "mykonnector", "account": "noid", "folder_to_save": "folderPath"}`,
+  COZY_FIELDS: `{"connector": "2fa-demo", "account": "noid", "folder_to_save": "folderPath"}`,
   DEBUG: '*'
 })
--- konnector.js
+++ konnector.js
@@ -3,7 +3,24 @@
 
 'use strict'
 
-const {baseKonnector} = require('cozy-konnector-libs')
+const pkg = require('./package.json')
+const fixtures = require('./data/fixture.json')
+
+const fs = require('fs')
+const path = require('path')
+const uuidv4 = require('uuid/v4')
+const uuidv5 = require('uuid/v5')
+const otp = require('otp')({
+  name: pkg.name,
+  secret: fixtures.otp.secret,
+  keySize: 16
+})
+
+const {baseKonnector, log, errors} = require('cozy-konnector-libs')
+
+const mocks = {
+  fields: require('./mocks/fields.json')
+}
 
 module.exports = baseKonnector.createNew({
   name: 'io.cozy.konnectors.mock',
@@ -15,10 +32,94 @@ module.exports = baseKonnector.createNew({
   // data : another object passed accross function, not used
   // next : this is a callback you have to call when the task of the current function is finished
   fetchOperations: [
+    preauth,
     auth
   ]
 })
 
-function auth (requiredFields, entries, data, next) {
+/**
+ * When executing the konnector in a standalone environment, we build the values
+ * that should be passed by Collect when re-executing the konnector for token
+ * submission, i.e. the context and TOTP token.
+ */
+function preauth (requiredFields, entries, data, next) {
+  if (process.env.NODE_ENV !== 'standalone' || !requiredFields.twofactor) return next()
+  requiredFields.twofactor = JSON.parse(Buffer.from(requiredFields.twofactor, 'base64').toString('ascii'))
+  requiredFields.token = otp.totp()
+  next()
+}
 
+function auth (requiredFields, entries, data, next) {
+  if (requiredFields.twofactor) {
+    check2fa(requiredFields.twofactor, requiredFields.token)
+    .then(() => resetContext(requiredFields))
+    .then(next())
+    .catch(err => {
+      log('error', err)
+      next('LOGIN_FAILED')
+    })
+  } else if (requiredFields.login === mocks.fields.login && requiredFields.password === mocks.fields.password) {
+    mock2fa()
+    .then(context => errors.requireTwoFactor(context))
+    .then(encodedContext => process.env.NODE_ENV === 'standalone' && saveEncodedContext(requiredFields, encodedContext))
+    .then(() => next('TWO_FACTOR_TOKEN_REQUESTED'))
+    .catch(err => {
+      log('error', err)
+      next('UNKNOWN_ERROR')
+    })
+  } else {
+    log('error', 'Incorrect credentials')
+    next('LOGIN_FAILED')
+  }
+}
+
+function mock2fa (callback) {
+  const context = {
+    _csrf: uuidv4(),
+    SESSIONID: uuidv5('https://mocks.cozy.io/konnectors/2fa', uuidv5.URL)
+  }
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path.resolve(__dirname, './mocks/twofactor.json'), JSON.stringify(context), err => {
+      err ? reject(err) : resolve(context)
+    })
+  })
+}
+
+function check2fa (context, token) {
+  const twofactor = require('./mocks/twofactor.json')
+  return new Promise((resolve, reject) => {
+    if (twofactor._csrf !== context._csrf || twofactor.SESSIONID !== context.SESSIONID) {
+      return reject(new Error('Invalid Context'))
+    }
+
+    return token === otp.totp() ? resolve() : reject(new Error('Invalid TOTP token'))
+  })
+}
+
+function saveEncodedContext (fields, encodedContext) {
+  return new Promise((resolve, reject) => {
+    const envFields = {
+      login: fields.login,
+      password: fields.password,
+      twofactor: encodedContext
+    }
+
+    fs.writeFile(path.resolve(__dirname, './data/env_fields.json'), JSON.stringify(envFields), err => {
+      err ? reject(err) : resolve(envFields)
+    })
+  })
+}
+
+function resetContext (fields) {
+  return new Promise((resolve, reject) => {
+    const envFields = {
+      login: fields.login,
+      password: fields.password
+    }
+
+    fs.writeFile(path.resolve(__dirname, './data/env_fields.json'), JSON.stringify(envFields), err => {
+      err ? reject(err) : resolve(envFields)
+    })
+  })
 }
